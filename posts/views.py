1e69b9428a3874d346d6c6b863398f1a2f3892b2
banumariwan <mariwanburhan5@gmail.com>
2025-11-16 04:57:35 +0300

    Save current changes before pulling

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,10 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from .models import Post
 from .serializers import PostSerializer
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all().order_by('-created_at')
     serializer_class = PostSerializer
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['title', 'content']        # searchable fields
+    ordering_fields = ['created_at', 'title']   # allowed ordering
